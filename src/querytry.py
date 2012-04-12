753c38f827593269d0ba985e19fa2552c2230d95
Rebecca Ackerman <rcackerman@gmail.com>
2012-04-12 01:08:31 -0500

    Updating querytry.py, to create a class.

--- src/querytry.py
+++ src/querytry.py
@@ -1,19 +1,37 @@
 from google.appengine.api import users
 from google.appengine.ext import db
 
-def list_provs():
-  provs = Providers.all()
-  for prov in provs:
-    print prov.pAgency
-  return provs
-    
-def list_addresses(agency):
-  addrs = []
-  addresses = db.Query(Providers).filter('pAgency =', agency)
-  for addr in addresses:
-    print prov.pAddress
-  return None
+class Providers(db.Model):
+	"""Provider Information DB"""
+	pDomain = db.StringProperty()
+	pType = db.StringProperty()
+	pAgency = db.StringProperty()
+	pSite = db.StringProperty()
+	progamName = db.StringProperty()
+	pAddress = db.StringProperty()
+	pAddress2 = db.StringProperty()
+	pCity = db.StringProperty()
+	pState = db.StringProperty()
+	pZip = db.StringProperty()
+	pPhone = db.StringProperty()
+	pEmail = db.StringProperty()
+	pNotes = db.TextProperty()
+	Owner = db.UserProperty()
 
-providers = list_provs()
-for provider in providers:
-  print list_addresses(provider)
+	def list_provs(self):
+		self.providers = []
+		self.provs = Providers.all()
+		for prov in self.provs:
+			self.providers.append(prov.pAgency)
+                print self.providers
+		return self.providers
+	
+	def list_addresses(self, providers):
+	    print providers
+		self.addrs = []
+		self.addresses = db.Query(Providers).filter('pAgency =', provider)
+		for addr in self.addresses:
+			addrs.append(addr.pAddress)
+		return addrs
+		
+Providers.list_addresses()
